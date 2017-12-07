f0ce2826c186a271a055c593f1fe30755d5ec5ee
Corvey <corvey@163.com>
2017-12-07 21:05:07 +0800

    日常刷题

---
+++ ACM/约瑟夫问题/main.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int die = 0, time = 0;
+    int people[41] = {0};
+    for(int i=0; die<39; i++)
+    {
+        i %= 41;
+        if(people[i])
+            continue;
+        time++;
+        if(time==3)
+        {
+            time = 0;
+            printf("%d->", i+1);
+            people[i] = 1;
+            ++die;
+        }
+    }
+    return 0;
+}
