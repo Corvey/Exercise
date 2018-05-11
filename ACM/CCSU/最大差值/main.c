e33b83eeb2839dd8391eb35ac989e5a1d5440235
Corvey <corvey@163.com>
2018-05-11 22:03:11 +0800

    2018年5月11日22:03:11

---
+++ ACM/CCSU/最大差值/main.c
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int n, a[100];
+    while(~scanf("%d", &n))
+    {
+        ;
+    }
+}
---
+++ ACM/NYOJ/C小加 之 级数求和/main.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int n, k, i;
+    double s;
+    scanf("%d", &n);
+    while(n--)
+    {
+        scanf("%d", &k);
+        s = 0;
+        for(i=0; s<k; )
+            s += 1/(double)(++i);
+        printf("%d\n", i);
+    }
+}
