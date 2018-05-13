7a8ccc61c5f68f7cdf55f433b149bdd44bec6866
Corvey <corvey@163.com>
2018-05-13 16:52:13 +0800

    whatever

---
+++ ACM/NYOJ/king VS king/main.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int t, n, a[100], b[100], as, bs;
+    scanf("%d", &t);
+    for(int i=0; i<t; ++i)
+    {
+        scanf("%d", &n);
+        for(int j=0; j<n; ++j)
+            scanf("%d", a+j);
+        for(int j=0; j<n; ++j)
+            scanf("%d", b+j);
+        for(int j=0; j<n-1; ++j)
+            for(int k=0; k<n-1-j; ++k)
+                if(a[k] > a[k+1])
+                    a[k] ^= a[k+1], a[k+1] ^= a[k], a[k] ^= a[k+1];
+        for(int j=0; j<n-1; ++j)
+            for(int k=0; k<n-1-j; ++k)
+                if(b[k] > b[k+1])
+                    b[k] ^= b[k+1], b[k+1] ^= b[k], b[k] ^= b[k+1];
+        as = bs = 0;
+        for(int j=0; j<n; ++j)
+        {
+            if(a[j] != b[j])
+            {
+                as += a[j]>b[j] ? 2 : 0;
+                bs += a[j]>b[j] ? 0 : 2;
+            }
+        }
+        if(as == bs)
+            printf("none\n");
+        else if(as > bs)
+            printf("X\n");
+        else
+            printf("Y\n");
+    }
+}
---
+++ ACM/NYOJ/king 选 太子/main.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int a[15], n, m;
+    scanf("%d", &m);
+    while(m--)
+    {
+        scanf("%d", &n);
+        for(int i=0; i<n; ++i)
+            scanf("%d", a+i);
+        for(int i=0; i<n-1; ++i)
+            for(int j=0; j<n-1-i; ++j)
+                if(a[j] > a[j+1])
+                    a[j] ^= a[j+1], a[j+1] ^= a[j], a[j] ^= a[j+1];
+        if(n&1)
+            printf("%d\n", a[(n-1)/2]);
+        else
+            printf("%d\n", a[(n-1)/2+1]);
+    }
+}
