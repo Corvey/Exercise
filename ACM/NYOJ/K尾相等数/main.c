2d081dadee192ae49be8ef817c6b3384db75b0b6
Corvey <corvey@163.com>
2018-05-19 10:53:19 +0800

    刷题

---
+++ ACM/NYOJ/K尾相等数/main.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+    int n, x, sum;
+    scanf("%d", &n);
+    while(n--)
+    {
+        scanf("%d", &x);
+        if(x==10)
+            printf("7\n");
+        else
+        {
+            x %= 1000;
+            for(int i=1; i<=9; ++i)
+            {
+
+            }
+        }
+    }
+}
