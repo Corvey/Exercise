5292a46aa94a4e912621a41ecf8fb24cc3cb7b22
Corvey <corvey@163.com>
2018-05-20 14:19:20 +0800

    刷题

---
+++ ACM/NYOJ/子串和/main.c
@@ -0,0 +1,57 @@
+﻿#include <stdio.h>
+int a[1000010], b[1000010];
+int main(void)
+{
+    int n, m, max, x, y, j;
+    scanf("%d", &n);
+    for(int i=0; i<n; ++i)
+    {
+        scanf("%d", &m);
+        for(j=0; j<m; ++j)
+            scanf("%d", a+j);
+        for(j=0; a[j]<=0 && j<m; ++j);
+        b[j] = a[j];
+        max = a[j];
+        x = j;
+        //printf("j:%d\n", j);
+        for(++j; j<m; ++j)
+        {
+            b[j] = b[j-1] + a[j];
+            if(max <= b[j])
+            {
+                max = b[j];
+                x = j;
+            }
+        }
+        //for(j=m-1; a[j]<=0 && j>=0; --j);
+        j = m-1;
+        max = a[j];
+        y = j;
+        //printf("j:%d\n", j);
+        for(--j; j>=0; --j)
+        {
+            a[j] += a[j+1];
+            if(max <= a[j])
+            {
+                max = a[j];
+                y = j;
+            }
+        }
+        ///*
+        for(j=0; j<m; ++j)
+        {
+            printf("%d ", b[j]);
+        }
+        printf("\n");
+        for(j=0; j<m; ++j)
+        {
+            printf("%d ", a[j]);
+        }
+        printf("\n");
+        //*/
+        if(x==n)
+            printf("%d\n", max);
+        else
+            printf("%d\n", max - a[x+1]);
+    }
+}
---
+++ ACM/NYOJ/寻找最大数（三）/main.c
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    char s[20];
+    int t, max, maxtag;
+    while(~scanf("%s %d", s, &t))
+    {
+        //getchar();
+        if(!t)
+        {
+            puts(s);
+            continue;
+        }
+        for(int i=0; s[i]; ++i)
+        {
+            if(s[i] == '9')
+                continue;
+            max = s[i];
+            maxtag = i;
+            for(int j=i+1; j-i <= t && s[j]; ++j)
+            {
+                if(s[j] > max)
+                {
+                    max = s[j];
+                    maxtag = j;
+                }
+            }
+            if(max == s[i])
+                continue;
+            else
+            {
+                for(int j=maxtag; j>=i+1; --j)
+                    s[j] ^= s[j-1], s[j-1] ^= s[j], s[j] ^= s[j-1];
+                t -= maxtag - i;
+            }
+            if(!t)
+                break;
+        }
+        puts(s);
+    }
+}
