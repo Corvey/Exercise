b1330f86b3c9a0b64b5af6b23b83841e27e241ee
Corvey <corvey@163.com>
2018-05-23 21:56:23 +0800

    whatever

---
+++ ACM/NYOJ/小明求素数积/main.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <math.h>
+int judge(int x)
+{
+    if(x==1)
+        return 0;
+    int a = sqrt(x+1);
+    for(int i=2; i<=a; ++i)
+    {
+        if(x%i == 0)
+           return 0;
+    }
+    return 1;
+}
+int main(void)
+{
+    int t, n, x;
+    scanf("%d", &t);
+    for(int i=0; i<t; ++i)
+    {
+        scanf("%d", &n);
+        x = 1;
+        for(int j=2; j<=n; ++j)
+        {
+            if(judge(j))
+                x = x%1000000 * j;
+        }
+        printf("%d\n", x%1000000);
+
+    }
+}
---
+++ ACM/NYOJ/小明的烦恼/main.c
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int n;
+    char s[12];
+    scanf("%d", &n);
+    for(int i=0; i<n; ++i)
+    {
+        scanf("%s", s);
+        getchar();
+        for(int j=0; j<11; ++j)
+        {
+            if(s[j] >= 'w')
+                printf("9");
+            else if(s[j] >= 't')
+                printf("8");
+            else if(s[j] >= 'p')
+                printf("7");
+            else if(s[j] >= 'm')
+                printf("6");
+            else if(s[j] >= 'j')
+                printf("5");
+            else if(s[j] >= 'g')
+                printf("4");
+            else if(s[j] >= 'd')
+                printf("3");
+            else if(s[j] >= 'a')
+                printf("2");
+        }
+        printf("\n");
+    }
+}
