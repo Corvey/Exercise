4eb88ae45efd24889c7cc806b306294f1fad0746
Corvey <corvey@163.com>
2018-05-25 22:38:25 +0800

    2018年5月25日22:38:25

---
+++ ACM/NYOJ/小明的调查作业/main.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int n, a[1000], t;
+    scanf("%d", &n);
+    for(int i=0; i<n; ++i)
+        scanf("%d", a+i);
+    for(int i=0; i<n-1; ++i)
+        for(int j=0; j<n-1-i; ++j)
+            if(a[j] > a[j+1])
+                a[j] ^= a[j+1], a[j+1] ^= a[j], a[j] ^= a[j+1];
+    t = 0;
+    for(int i=0; i<n; ++i)
+        if(a[i] != a[i+1])
+            ++t;
+    printf("%d\n", t);
+    for(int i=0; i<n; ++i)
+        if(a[i] != a[i+1])
+            printf("%d ", a[i]);
+    printf("\n");
+}
---
+++ ACM/NYOJ/小明的调查统计/main.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int t, n, a[100];
+    scanf("%d", &t);
+    while(t--)
+    {
+        scanf("%d", &n);
+        for(int i=0; i<n; ++i)
+            scanf("%d", a+i);
+        for(int i=0; i<n-1; ++i)
+            for(int j=0; j<n-1-i; ++j)
+                if(a[j] > a[j+1])
+                    a[j] ^= a[j+1], a[j+1] ^= a[j], a[j] ^= a[j+1];
+        printf("%d\n", a[n-1] - a[0]);
+    }
+}
