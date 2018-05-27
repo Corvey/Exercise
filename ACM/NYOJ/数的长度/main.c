0500208d272e948e1286fab7821bbbf3b5e7101b
Corvey <corvey@163.com>
2018-05-27 13:42:27 +0800

    刷题

---
+++ ACM/NYOJ/数的长度/main.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <math.h>
+#define pi 3.1415926536
+#define E 2.71828182846
+int main(void)
+{
+    int m;
+    double n, ans;
+    scanf("%d", &m);
+    for(int i=0; i<m; ++i)
+    {
+        scanf("%lf", &n);
+        ans = log10(2*pi*n)/2 + n * log10(n/E);
+        printf("%d\n", (int)ans+1);
+    }
+}
---
+++ ACM/NYOJ/最大值和最小值/main.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int n, count;
+    char s[101];
+    scanf("%d\n", &n);
+    while(n--)
+    {
+        for(count=0; (s[count] = getchar())!='\n'; ++count);
+        s[count] = '\0';
+        if(s[0] == '0')
+            printf("0 0\n");
+        else
+        {
+            for(int i=0; i<count-1; ++i)
+                for(int j=0; j<count-i-1; ++j)
+                    if(s[j] < s[j+1])
+                        s[j] ^= s[j+1], s[j+1] ^= s[j], s[j] ^= s[j+1];
+            printf("%s ", s);
+            for(int i=count-1; i>=0; --i)
+                if(s[i] != '0')
+                    putchar(s[i]);
+            putchar('\n');
+        }
+    }
+}
