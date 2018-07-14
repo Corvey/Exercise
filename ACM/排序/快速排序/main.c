a220283e50d557017ff72476b5e8c81541bd8a23
Corvey <corvey@163.com>
2018-07-14 19:23:14 +0800

    刷题

---
+++ ACM/排序/快速排序/main.c
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#define MAX 10
+
+int FindPos(int *n, int low, int high)
+{
+    int temp = n[low];
+    do
+    {
+        for(; temp < n[high]; --high)
+        {
+            if(low == high)
+                return low;
+        }
+        n[low] = n[high];
+        for(; low < high && temp > n[low]; ++low);
+        n[high] = n[low];
+
+
+    } while(low < high);
+    n[high] = temp;
+    return low;
+}
+
+void QuickSort(int *n, int low, int high)
+{
+    if(low < high)
+    {
+        int pos = FindPos(n, low, high);
+        QuickSort(n, low, pos-1);
+        QuickSort(n, pos+1, high);
+    }
+}
+
+int main(void)
+{
+    int n[MAX];
+    for(int i=0; i<MAX; i++)
+    {
+        printf("请输入第%d个数据：", i+1);
+        scanf("%d", &n[i]);
+    }
+    QuickSort(n , 0, MAX-1);
+
+    printf("\n排序后：\n");
+    for(int i=0; i<MAX; i++)
+    {
+        printf("%d ", n[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
---
+++ ACM/排序/直接插入排序/main.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int n[10], i, j, k, temp;
+    for(i=9; i>=0; i--)
+    {
+        n[i] = 10-i;
+    }
+
+    for(i = 0; i < 9; i++)
+    {
+        if(n[i] > n[i+1])
+        {
+            temp = n[i+1];
+            for(j = i; j >= 0 && temp < n[j]; j--)
+            {
+                n[j + 1] = n[j];
+            }
+            n[j + 1] = temp;
+        }
+    }
+
+    for(i=0; i<10; i++)
+    {
+        printf("%d\n", n[i]);
+    }
+}
