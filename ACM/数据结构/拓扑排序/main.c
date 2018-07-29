fe91a6c35b608bebcca0733dd029e8b21c13bbfb
Corvey <corvey@163.com>
2018-07-29 21:21:29 +0800

    whatever

---
+++ ACM/数据结构/拓扑排序/main.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct graph
+{
+    int tag;
+    int indegree;
+    struct node *pNext;
+}GRAPH, *PGRAPH;
+
+typedef struct node
+{
+    int tag;
+    struct node *pNext;
+}NODE, *PNODE;
+
+PGRAPH InitGraph(void)
+{
+    int n;
+    printf("")
+
+}
+
+int main(void)
+{
+
+}
