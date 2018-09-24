ef3b3f8167c0c05914b51a3a3a5713dbef98f304
Corvey <corvey@163.com>
2018-09-24 13:36:24 +0800

    whatever

---
+++ ACM/NYOJ/计算几何/三点顺序/main.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int x1, y1, x2, y2, x3, y3;
+    while(1)
+    {
+        scanf("%d%d%d%d%d%d", &x1, &y1, &x2, &y2, &x3, &y3);
+        if(!x1 && !y1 && !x2 && !y2 && !x3 && !y3)
+            break;
+        printf("%d\n", (x2-x1)*(y3-y1) - (x3-x1)*(y2-y1) > 0 ? 0 : 1);
+    }
+}
---
+++ ACM/NYOJ/计算几何/三角形面积/main.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int x1, x2, x3, y1, y2, y3;
+    double s;
+    while(1)
+    {
+        scanf("%d%d%d%d%d%d", &x1, &y1, &x2, &y2, &x3, &y3);
+        if(!x1 && !y1 && !x2 && !y2 && !x3 && !y3)
+            break;
+        s = (double)((x1*y2-x2*y1)+(x2*y3-x3*y2)+(x3*y1-x1*y3));
+        s /= 2;
+        if(s>0)
+            printf("%.1lf\n", s);
+        else
+            printf("%.1lf\n", s*(-1));
+    }
+}
