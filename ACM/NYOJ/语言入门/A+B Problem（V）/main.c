2be5379dea9e0d630ac390424d871f9b275f6cc4
Corvey <corvey@163.com>
2018-10-04 11:35:04 +0800

    日常刷题

---
+++ ACM/NYOJ/语言入门/A+B Problem（V）/main.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+    int a, b, sum, lena, lenb;
+    scanf("%d%d", &a, &b);
+    while(a && b)
+    {
+        sum = 0;
+        for(lena = log10(a); a; a/=10, --lena)
+            sum += a%10 * pow(10, lena);
+        for(lenb = log10(b); b; b/=10, --lenb)
+            sum += b%10 * pow(10, lenb);
+        printf("%d\n", sum);
+        scanf("%d%d", &a, &b);
+    }
+}
