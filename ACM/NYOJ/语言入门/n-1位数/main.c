c76f0ebbf51ce62dc9fbb8f5f519ce4d66a4f5c1
Corvey <corvey@163.com>
2018-10-09 23:27:09 +0800

    提交若干

---
+++ ACM/NYOJ/语言入门/n-1位数/main.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+    int n, x, j;
+    scanf("%d", &n);
+    for(int i=0; i<n; ++i)
+    {
+        scanf("%d", &x);
+        for(j=0; x/(int)pow(10, j+1) != 0; ++j);
+        printf("%d\n", x%(int)pow(10, j));
+    }
+}
---
+++ ACM/NYOJ/语言入门/万圣节派对/main.c
@@ -0,0 +1,71 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main(void)
+{
+    int n, tag;
+    char a[7], b[7];
+    scanf("%d", &n);
+    for(int i=0; i<n; ++i)
+    {
+        getchar();
+        scanf("%s%s", a, b);
+        while(strcmp(a, b))
+        {
+            tag = 1;
+            for(int j=0; j<5; ++j)
+            {
+                if(abs(a[j]-a[j+1]) > 4)
+                {
+                    tag = 0;
+                    break;
+                }
+                if(j<=3)
+                {
+                    if(a[j] == a[j+1] && a[j+1] == a[j+2])
+                    {
+                        tag = 0;
+                        break;
+                    }
+                }
+            }
+            if(tag)
+                printf("%s\n", a);
+            tag = 5;
+            while(1)
+            {
+                ++a[tag];
+                if(a[tag] > '6')
+                {
+                    a[tag] = '0';
+                    --tag;
+                }
+                else
+                    break;
+            }
+        }
+        if(!strcmp(a, b))
+        {
+            tag = 1;
+            for(int j=0; j<5; ++j)
+            {
+                if(abs(a[j]-a[j+1]) > 4)
+                {
+                    tag = 0;
+                    break;
+                }
+                if(j<=3)
+                {
+                    if(a[j] == a[j+1] && a[j+1] == a[j+2])
+                    {
+                        tag = 0;
+                        break;
+                    }
+                }
+            }
+            if(tag)
+                printf("%s\n", a);
+        }
+        printf("\n");
+    }
+}
