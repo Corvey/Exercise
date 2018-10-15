cbc38abf9339814caed8e1598af326f3e179c6ad
Corvey <corvey@163.com>
2018-10-15 23:39:15 +0800

    刷题

---
+++ ACM/NYOJ/语言入门/不可以！/main.c
@@ -0,0 +1,17 @@
+﻿
+int main(void)
+{
+    int x, y;
+    while(scanf("%d%d", &x, &y) != EOF)
+    {
+        if(x && y)
+        {
+            if(x * y - abs(x*y))
+                printf("Signs are opposite\n");
+            else
+                printf("Signs are not opposot\n");
+        }
+        else
+            printf("Signs can't be sure\n");
+    }
+}
---
+++ ACM/NYOJ/语言入门/两点距离/main.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+    int n;
+    double a, b, x, y;
+    scanf("%d", &n);
+    for(int i=0; i<n; ++i)
+    {
+        scanf("%lf%lf%lf%lf", &a, &b, &x, &y);
+        a -= x;
+        b -= y;
+        printf("%.2lf\n", sqrt(a*a+b*b));
+    }
+}
