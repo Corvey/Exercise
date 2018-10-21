a9e388490a94f68d57169292d44a0afc54cb9406
Corvey <corvey@163.com>
2018-10-21 23:18:21 +0800

    日常

---
+++ ACM/NYOJ/语言入门/国王的魔镜/main.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <string.h>
+int judge(char *s, int len)
+{
+    if(len&1)
+        return 0;
+    for(int i=0; i<len-1; ++i, --len)
+    {
+        if(s[i] != s[len-1])
+            return 0;
+    }
+    return 1;
+}
+int main(void)
+{
+    int n, len;
+    char s[101];
+    scanf("%d", &n);
+    for(int i=0; i<n; ++i)
+    {
+        getchar();
+        scanf("%s", s);
+        len = strlen(s);
+        while(judge(s, len))
+        {
+            len /= 2;
+            memset(s+len, '\0', len*sizeof(char));
+        }
+        printf("%d\n", len);
+    }
+}
---
+++ ACM/NYOJ/语言入门/大小写互换/main.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int n;
+    char s[101];
+    scanf("%d", &n);
+    for(int i=0; i<n; ++i)
+    {
+        scanf("%s", s);
+        for(int j=0; s[j] != '\0'; ++j)
+        {
+            if(s[j] <= 'Z')
+            {
+                s[j] += 32;
+            }
+            else
+                s[j] -= 32;
+        }
+        puts(s);
+    }
+}
