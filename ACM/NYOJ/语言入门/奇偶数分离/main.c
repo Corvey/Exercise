b75684ea44663ef6023df78536cc2f3f8215063c
Corvey <corvey@163.com>
2018-10-23 23:10:23 +0800

    日常刷题

---
+++ ACM/NYOJ/语言入门/奇偶数分离/main.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int n, x;
+    scanf("%d", &n);
+    for(int i=0; i<n; ++i)
+    {
+        scanf("%d", &x);
+        for(int j=1; j<=x; ++j)
+        {
+            if(j&1)
+                printf("%d ", j);
+        }
+        printf("\n");
+        for(int j=2; j<=x; ++j)
+        {
+            if(!(j&1))
+                printf("%d ", j);
+        }
+        printf("\n");
+    }
+}
---
+++ ACM/NYOJ/语言入门/奋斗的小蜗牛/main.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int n, h, t;
+    scanf("%d", &n);
+    for(int i=0; i<n; ++i)
+    {
+        scanf("%d", &h);
+        h -= 10;
+        if(h<=0)
+            printf("1\n");
+        else
+        {
+            t = h/5 + 1;
+            printf("%d\n", t+(h%5>0));
+        }
+
+    }
+}
