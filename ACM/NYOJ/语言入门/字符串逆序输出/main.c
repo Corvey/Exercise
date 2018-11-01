9152fd6e0f0c1778bd7778858190de1fdc7f8256
Corvey <corvey@163.com>
2018-11-01 22:55:01 +0800

    whatever

---
+++ ACM/NYOJ/语言入门/字符串逆序输出/main.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+char s[41];
+int abc(int n)
+{
+    if(s[n] == '\0')
+        return 1;
+    else if(s[n] == ' ' || s[n]>='0' && s[n]<='9')
+        return abc(n+1);
+    else
+    {
+        abc(n+1);
+        printf("%c", s[n]);
+    }
+}
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+    getchar();
+    for(int i=0; i<n; ++i)
+    {
+        gets(s);
+        abc(0);
+        printf("\n");
+    }
+}
---
+++ ACM/NYOJ/语言入门/对决/main.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+    double a, x, y;
+    while(1)
+    {
+        scanf("%lf%lf", &x, &y);
+        if(!x && !y)
+            break;
+        if(y < x-1 || 4*y > x*x)
+            printf("NO\n");
+        else
+        {
+            a = (x+sqrt(x*x-4*y))/2;
+            if((int)a == a)
+                printf("YES\n");
+            else
+                printf("NO\n");
+        }
+    }
+}
