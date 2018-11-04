a67e240fd24b5d95e94ca5f02d7bcf49cfcdd48a
Corvey <corvey@163.com>
2018-11-04 16:02:04 +0800

    2018年11月4日16:02:04

---
+++ ACM/NYOJ/语言入门/小光棍数/main.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int n, j, t;
+    double x, count;
+    scanf("%d", &n);
+    for(int i=0; i<n; ++i)
+    {
+        scanf("%lf", &x);
+        if(x==1)
+            printf("471\n");
+        else
+            printf("%.0lf471\n", x-1);
+    }
+}
