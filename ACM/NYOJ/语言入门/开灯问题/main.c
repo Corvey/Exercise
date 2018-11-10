5ac4c966d191058f94f18b4e3a6c69ad2af48a08
Corvey <corvey@163.com>
2018-11-10 22:22:10 +0800

    日常

---
+++ ACM/NYOJ/语言入门/开灯问题/main.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int a[1001], n, k;
+    scanf("%d%d", &n, &k);
+    for(int i=1; i<=n; ++i)
+        a[i] = 0;
+
+    for(int i=1; i<=k; ++i)
+    {
+        for(int j=1; j<=n; ++j)
+        {
+            if(j%i == 0)
+                a[j] = ~a[j];
+        }
+    }
+    for(int i=1; i<=n; ++i)
+    {
+        if(a[i])
+            printf("%d ", i);
+    }
+    printf("\n");
+}
