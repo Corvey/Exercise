ed21d14d1d251252d03511ec26ea1d2ad778dd4f
Corvey <corvey@163.com>
2018-11-10 22:19:10 +0800

    2018年11月10日22:19:10

---
+++ ACM/NYOJ/语言入门/小学生算术/main.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int m, n, count, t;
+    while(1)
+    {
+        scanf("%d%d", &m, &n);
+        if(!m && !n)
+            break;
+        count = 0;
+        t = m%10 + n%10;
+        if(t>9)
+            ++count;
+        t = m/10%10 + n/10%10 + t/10;
+        if(t>9)
+            ++count;
+        t = m/100 + n/100 + t/10;
+        if(t>9)
+            ++count;
+        printf("%d\n", count);
+    }
+}
---
+++ ACM/NYOJ/语言入门/就我不坑（未竟）/main.c
@@ -0,0 +1,194 @@
+﻿/*
+void qwe(char *s, int n)
+{
+    switch (n)
+    {
+    case 1:
+        strcat(s, "壹");
+        break;
+
+    case 2:
+        strcat(s, "贰");
+        break;
+
+    case 3:
+        strcat(s, "叁");
+        break;
+
+    case 4:
+        strcat(s, "肆");
+        break;
+
+    case 5:
+        strcat(s, "伍");
+        break;
+
+    case 6:
+        strcat(s, "陆");
+        break;
+
+    case 7:
+        strcat(s, "柒");
+        break;
+
+    case 8:
+        strcat(s, "捌");
+        break;
+
+    case 9:
+        strcat(s, "玖");
+        break;
+
+    default:
+        break;
+    }
+}
+void asd(char *s, int n)
+{
+    switch (n)
+    {
+    case 8:
+        strcat(s, "亿");
+        break;
+
+    case 4:
+        strcat(s, "万");
+        break;
+
+    case 7:
+    case 3:
+        strcat(s, "仟");
+        break;
+
+    case 6:
+    case 2:
+        strcat(s, "佰");
+        break;
+
+    case 9:
+    case 5:
+    case 1:
+        strcat(s, "拾");
+        break;
+
+    default:
+        break;
+    }
+}
+*/
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+void qwe(int n)
+{
+    switch (n)
+    {
+    case 1:
+        printf("壹");
+        break;
+
+    case 2:
+        printf("贰");
+        break;
+
+    case 3:
+        printf("叁");
+        break;
+
+    case 4:
+        printf("肆");
+        break;
+
+    case 5:
+        printf("伍");
+        break;
+
+    case 6:
+        printf("陆");
+        break;
+
+    case 7:
+        printf("柒");
+        break;
+
+    case 8:
+        printf("捌");
+        break;
+
+    case 9:
+        printf("玖");
+        break;
+
+    default:
+        break;
+    }
+}
+void asd(int n)
+{
+    switch (n)
+    {
+    case 8:
+        printf("亿");
+        break;
+
+    case 4:
+        printf("万");
+        break;
+
+    case 7:
+    case 3:
+        printf("仟");
+        break;
+
+    case 6:
+    case 2:
+        printf("佰");
+        break;
+
+    case 9:
+    case 5:
+    case 1:
+        printf("拾");
+        break;
+
+    default:
+        break;
+    }
+}
+int main(void)
+{
+    int x, count, temp, pre = 0;
+    char a[19];
+    while(~scanf("%d", &x))
+    {
+        if(!x)
+        {
+            printf("零\n");
+            continue;
+        }
+        sprintf(a, "%d", x);
+        count = strlen(a);
+        while(x)
+        {
+            temp = x/pow(10, --count);
+            qwe(temp);
+            printf("temp:%d count:%d pre:%d\n", temp, count, pre);
+            if(temp)
+                pre = 0;
+            else if(!pre && count != 4 && count != 8)
+                printf("零"), pre = 1;
+            if(temp || count ==4 && !pre || count == 8 && !pre)
+                asd(count);
+            x -= temp * pow(10, count);
+            if(x < pow(10, count-1))
+            {
+                --count;
+                if(count > 8)
+                    printf("亿");
+                else if(count > 4)
+                    printf("万");
+            }
+        }
+        printf("\n");
+    }
+}
