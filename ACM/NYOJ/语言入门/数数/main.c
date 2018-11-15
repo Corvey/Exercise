e0a086e2ca8b7616d149c61bd759c63a036a5606
Corvey <corvey@163.com>
2018-11-15 23:46:15 +0800

    whatever

---
+++ ACM/NYOJ/语言入门/数数/main.c
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int n, x;
+    scanf("%d", &n);
+    for(int i=0; i<n; ++i)
+    {
+        scanf("%d", &x);
+        while(x)
+        {
+            switch (x%10)
+            {
+            case 0:
+            case 1:
+                printf("O");
+                break;
+            case 2:
+            case 3:
+                printf("T");
+                break;
+            case 4:
+            case 5:
+                printf("F");
+                break;
+            case 6:
+            case 7:
+                printf("S");
+                break;
+            case 8:
+                printf("E");
+                break;
+            case 9:
+                printf("N");
+                break;
+            default:
+                break;
+            }
+            x /= 10;
+        }
+        printf("\n");
+    }
+}
---
+++ ACM/NYOJ/语言入门/数数小木块/main.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int n, x, sum, last;
+    scanf("%d", &n);
+    for(int i=0; i<n; ++i)
+    {
+        scanf("%d", &x);
+        sum = 1;
+        last = 1;
+        for(int j=2; j<=x; ++j)
+        {
+            sum += last + j;
+            last += j;
+        }
+        printf("%d\n", sum);
+    }
+}
