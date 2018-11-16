2a069c47e153c7edce0e1eb99f3b7ac2bb132000
Corvey <corvey@163.com>
2018-11-16 21:50:16 +0800

    刷题

---
+++ ACM/NYOJ/语言入门/整除个数/main.c
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int n, x;
+    while(scanf("%d%d", &n, &x) != EOF)
+    {
+        printf("%d\n", n/x);
+    }
+}
---
+++ ACM/NYOJ/语言入门/无主之地1/main.c
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int m[100][2], x, n, j;
+    for(int i=0; i<100; ++i)
+    {
+        m[i][0] = 0;
+        m[i][1] = 0;
+    }
+
+    for(int i=0; ; ++i)
+    {
+        scanf("%d%d", &x, &n);
+        if(x && n)
+        {
+            for(j=0; j<i; ++j)
+            {
+                if(m[j][0] == x)
+                {
+                    m[j][1] += n;
+                    break;
+                }
+            }
+            if(j == i)
+            {
+                m[i][0] = x;
+                m[i][1] = n;
+            }
+
+        }
+        else
+            break;
+    }
+
+    for(int i=0; i<100; ++i)
+    {
+        if(m[i][0])
+            printf("%d %d\n", m[i][0], m[i][1]);
+    }
+}
