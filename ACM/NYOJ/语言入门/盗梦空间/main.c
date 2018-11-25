ce4c6aa0936010b564d15d19119a2cf4a7d6835b
Corvey <corvey@163.com>
2018-11-25 13:09:25 +0800

    日常

---
+++ ACM/NYOJ/语言入门/盗梦空间/main.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+    int t, n, time, k;
+    double sum;
+    char s[5];
+    scanf("%d", &t);
+    for(int i=0; i<t; ++i)
+    {
+        scanf("%d", &n);
+        k = 1;
+        sum = 0;
+        for(int j=0; j<n; ++j)
+        {
+            scanf("%s", s);
+            if(!strcmp(s, "IN"))
+                k *= 20;
+            else if(!strcmp(s, "STAY"))
+            {
+                scanf("%d", &time);
+                sum += (double)time/k;
+            }
+            else if(!strcmp(s, "OUT"))
+                k /= 20;
+        }
+        printf("%.0lf\n", sum*60);
+    }
+}
