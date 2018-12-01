1dae00e566bffb28993c7dfdae8c1550ac3bc9de
Corvey <corvey@163.com>
2018-12-01 11:03:01 +0800

    whatever

---
+++ ACM/NYOJ/语言入门/精　挑　细　选/main.c
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int n, m, l, d, code, maxl, mind, maxcode;
+    scanf("%d", &n);
+    for(int i=0; i<n; ++i)
+    {
+        scanf("%d", &m);
+        scanf("%d%d%d", &maxl, &mind, &maxcode);
+        for(int j=1; j<m; ++j)
+        {
+            scanf("%d%d%d", &l, &d, &code);
+            if(l > maxl)
+            {
+                maxl = l;
+                mind = d;
+                maxcode = code;
+            }
+            else if(l == maxl)
+            {
+                if(d < mind)
+                {
+                    maxl = l;
+                    mind = d;
+                    maxcode = code;
+                }
+                else if(d == mind)
+                {
+                    if(code > maxcode)
+                    {
+                        maxl = l;
+                        mind = d;
+                        maxcode = code;
+                    }
+                }
+            }
+        }
+        printf("%d\n", maxcode);
+    }
+}
---
+++ ACM/NYOJ/语言入门/素数/main.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <math.h>
+int judge(int x)
+{
+    if(x==1)
+        return 0;
+    int a = sqrt(x+1);
+    for(int i=2; i<=a; ++i)
+    {
+        if(x%i == 0)
+           return 0;
+    }
+    return 1;
+}
+int main(void)
+{
+    int n, x, ld, rd;
+    scanf("%d", &n);
+    for(int i=0; i<n; ++i)
+    {
+        scanf("%d", &x);
+        for(rd=x; !judge(rd); ++rd);
+        for(ld=x; !judge(ld); --ld);
+        if(rd-x <= x-ld)
+            printf("%d\n", rd);
+        else
+            printf("%d\n", ld);
+    }
+}
