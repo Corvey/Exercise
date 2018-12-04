bc9bd42e3f94d3007d5f810020b29f9f60e2b30c
Corvey <corvey@163.com>
2018-12-04 22:01:04 +0800

    whatever

---
+++ ACM/NYOJ/语言入门/谁是最好的Coder/main.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int n, a, b, max;
+    while(1)
+    {
+        scanf("%d", &n);
+        if(!n)
+            break;
+        max=0;
+        for(int i=0; i<n; ++i)
+        {
+            scanf("%d%d", &a, &b);
+            if(a+b > max)
+                max = a+b;
+        }
+        printf("%d\n", max);
+
+    }
+}
