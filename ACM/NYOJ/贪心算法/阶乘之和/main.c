c0046335317e7ec531322021f30f33e4e56cac0d
Corvey <corvey@163.com>
2018-12-14 22:13:14 +0800

    日常

---
+++ ACM/NYOJ/贪心算法/阶乘之和/main.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+int Factorial(int n, int a)
+{
+    n==1 ? a : Factorial(n-1, a*n);
+}
+int main(void)
+{
+    int a[10], m, n;
+    for(int i=1; i<10; ++i)
+        a[i] = Factorial(10-i, 1);
+    scanf("%d", &m);
+    for(int i=0; i<m; ++i)
+    {
+        scanf("%d", &n);
+        for(int i=1; i<10; ++i)
+        {
+            if(n < a[i])
+                continue;
+            else
+                n -= a[i];
+        }
+        if(!n)
+            printf("Yes\n");
+        else
+            printf("No\n");
+    }
+}
---
+++ ACM/NYOJ/贪心算法/非洲小孩/main.c
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    int a[100][2], n, temp, count;
+    while(~scanf("%d", &n))
+    {
+        for(int i=0; i<n; ++i)
+        {
+            a[i][0] = 0;
+            a[i][1] = 0;
+            scanf("%d", &temp);
+            a[i][0] += temp*100;
+            getchar();
+            scanf("%d", &temp);
+            a[i][0] += temp;
+            getchar();
+            scanf("%d", &temp);
+            a[i][1] += temp*100;
+            getchar();
+            scanf("%d", &temp);
+            a[i][1] += temp;
+            if(a[i][0] > a[i][1])
+                a[i][0] ^= a[i][1], a[i][1] ^= a[i][0], a[i][0] ^= a[i][1];
+        }
+        for(int i=0; i<n-1; ++i)
+        {
+            for(int j=0; j<n-1-i; ++j)
+            {
+                if(a[j][1] > a[j+1][1])
+                {
+                    a[j][0] ^= a[j+1][0], a[j+1][0] ^= a[j][0], a[j][0] ^= a[j+1][0];
+                    a[j][1] ^= a[j+1][1], a[j+1][1] ^= a[j][1], a[j][1] ^= a[j+1][1];
+                }
+            }
+        }
+        count = 0;
+        for(int i=0; i<n; ++count)
+            for(temp = a[i][1], ++i; i<n && temp >= a[i][0]; ++i);
+        printf("%d\n", count);
+    }
+}
